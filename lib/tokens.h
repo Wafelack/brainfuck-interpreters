5a383594bba3efa5014bc1ac311918319d55048c
Wafelack <wafelack@protonmail.com>
2020-11-02 19:35:46 +0100

    feat: added file reading

---
+++ lib/tokens.h
@@ -0,0 +1,16 @@
+#ifndef _TOKENS_H_
+#define _TOKENS_H_
+
+typedef enum
+{
+    LEFT_CHEVRON,
+    RIGHT_CHEVRON,
+    PLUS,
+    MINUS,
+    DOT,
+    COMA,
+    LEFT_BRACE,
+    RIGHT_BRACE
+}
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+#define USAGE -1
+#define NOT_FOUND -2
+
+int main(int argc, char **argv)
 {
-    puts("Hello, World !");
+    if (argc != 2)
+    {
+        printf("Usage : bf <filename>");
+        return USAGE;
+    }
+    FILE *fp;
+    if (fp = fopen(argv[1], "r"))
+    {
+        fseek(fp, 0L, SEEK_END);
+        const long size = ftell(fp) + 1;
+        rewind(fp);
+
+        char *buf = malloc(sizeof(char) * size);
+
+        fgets(buf, size, fp);
+
+        printf("%s", buf);
+    }
+    else
+    {
+        printf("File not found");
+        return NOT_FOUND;
+    }
+
     return 0;
 }
