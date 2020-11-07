40cdd36f0288406cfd7c0801d0ed66b162f00d8c
Wafelack <wafelack@protonmail.com>
2020-11-07 09:44:02 +0100

    feat: Writing scanner

---
+++ src/scanner/scanner.rs
@@ -0,0 +1,50 @@
+use crate::scanner::tokens::Token;
+
+pub struct Scanner {
+    pub source: String,
+    pub chars: Vec<char>,
+    pub start: usize,
+    pub current: usize,
+    pub tokens: Vec<Token>,
+}
+impl Scanner {
+    pub fn new(source: String) -> Scanner {
+        Scanner {
+            source: source.clone(),
+            chars: source.clone().chars().collect(),
+            start: 0,
+            current: 0,
+            tokens: vec![],
+        }
+    }
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+    fn add_token(&mut self, token: Token) {
+        self.tokens.push(token);
+    }
+    fn advance(&mut self) -> char {
+        self.current += 1;
+        self.chars[self.current - 1]
+    }
+    fn scan_token(&mut self) {
+        let c = self.advance();
+        match c {
+            '(' => self.add_token(Token::LeftParen),
+            ')' => self.add_token(Token::RightParen),
+            '[' => self.add_token(Token::LeftBrace),
+            ']' => self.add_token(Token::RightBrace),
+            '.' => self.add_token(Token::Dot),
+            ',' => self.add_token(Token::Coma),
+            '>' => self.add_token(Token::RightChevr),
+            '<' => self.add_token(Token::LeftChevr),
+            _ => (),
+        }
+    }
+    pub fn scan_tokens(&mut self) -> Vec<Token> {
+        while !self.is_at_end() {
+            self.scan_token();
+        }
+        self.tokens.clone()
+    }
+}
---
+++ src/scanner/tokens.rs
@@ -0,0 +1,13 @@
+#[derive(Clone)]
+pub enum Token {
+    LeftBrace,
+    RightBrace,
+    LeftParen,
+    RightParen,
+    Plus,
+    Minus,
+    Coma,
+    Dot,
+    LeftChevr,
+    RightChevr,
+}
